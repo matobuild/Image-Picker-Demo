6163be0682a71e7932c04b946e3f9050a33c5b59
Mato <tissuepaperarelimited@gmail.com>
2022-09-13 20:40:34 +0800

    Initial Commit

---
+++ Image Picker Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Image Picker Demo
+//
+//  Created by kittawat phuangsombat on 2022/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Image Picker Demo/Image_Picker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Picker_DemoApp.swift
+//  Image Picker Demo
+//
+//  Created by kittawat phuangsombat on 2022/9/13.
+//
+
+import SwiftUI
+
+@main
+struct Image_Picker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
